42eaae8f058ae5dfc75648d800d55d0f0f11c615
Michael C <michael@mchang.name>
2022-11-01 13:34:04 -0400

    [scriptlet] repc - add wfke

--- scriptlets.js
+++ scriptlets.js
@@ -6,19 +6,19 @@
     const selector = '{{1}}';
     const oldClass = '{{2}}';
     const newClass = '{{3}}';
+    const wfke = (selector, callback) => {
+        var el = document.querySelector(selector);
+        if (el) return callback();
+        setTimeout(wfke, 100, selector, callback);
+    }
     const addclass = ev => {
-        if (ev) { window.removeEventListener(ev.type, addclass, true);  }
         const nodes = document.querySelectorAll(selector);
         try {
-            for ( const node of nodes ) {
+            for (const node of nodes) {
                 node.classList.remove(oldClass);
                 node.classList.add(newClass);
             }
-        } catch { }
+        } catch {}
     };
-    if (document.readyState === 'loading') {
-            window.addEventListener('DOMContentLoaded', addclass, true);
-        } else {
-            addclass();
-        }
+    wfke(selector, addclass)
 })();
