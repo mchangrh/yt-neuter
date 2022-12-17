e98edb8584295f32817a36f4a9eaccd84aaba9e0
Michael C <michael@mchang.name>
2022-12-17 02:27:46 -0500

    [persist-ctrl] add gradient from bottom

--- userscripts/persist-ctrl.user.js
+++ userscripts/persist-ctrl.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name         YT Persist Controls
 // @namespace    yt-neuter
-// @version      1.0.0
+// @version      1.1.0
 // @description  Make youtube controls persist
 // @author       michael mchang.name
 // @match        https://www.youtube.com/watch*
 // @icon         https://www.youtube.com/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/mchangrh/yt-neuter/main/userscripts/persist-ctrl.user.js
 // @downloadURL  https://raw.githubusercontent.com/mchangrh/yt-neuter/main/userscripts/persist-ctrl.user.js
-// @grant        none
+// @grant        GM_addStyle
 // @run-at       document-end
 // ==/UserScript==
 
 /* ubo rule:
 youtube.com##.ytp-chrome-bottom:style(opacity: 1 !important;)
+youtube.com##.ytp-gradient-bottom:style(opacity: 1 !important; display: block !important; )
 */
 
 GM_addStyle(".ytp-chrome-bottom { opacity: 1 !important; }");
+GM_addStyle(".ytp-gradient-bottom { opacity: 1 !important; display: block !important; }");
